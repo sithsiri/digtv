1c6d05f1352e28acb4ab015673dc77398a7b1513
sithsiri <sith@live.com>
2023-03-23 15:23:20 -0500

    remove load check

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ var slides = document.getElementsByClassName("mySlides");
 carousel();
 
 function carousel() {
-  if (!slides[slideIndex].complete) {
-    slideIndex++;
-    if (slideIndex >= slides.length) {slideIndex = 0;}
-    setTimeout(carousel, 100); // wait a little before checking the next image
-    return;
-  }
-  
   for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"; 
   }
